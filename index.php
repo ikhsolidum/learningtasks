b4db2343ee3448f6f3d64731778fefde7c832262
ikhsolidum <100141624+ikhsolidum@users.noreply.github.com>
2022-03-15 23:02:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 <h1> Welcome to My Website! I am Isaiah Kyle H. Solidum!</h1>
 <ul>
 	<li><a href="index.php">HOME</a></li>
-	<li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-	<li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+	<li><a href="/LT2/LT2.html">LEARNING TASK 2</a></li>
+	<li><a href="/LT3/LT3.html">LEARNING TASK 3</a></li>
 </ul>
 </body>
 </html>
